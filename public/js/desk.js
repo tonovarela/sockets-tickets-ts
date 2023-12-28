b2226fb138d98078d14466b19de12b73fcac4c9c
tonovarela <tonovarela@live.com>
2023-12-28 10:00:53 -0600

    Pantalla de escritorio lista

--- public/js/desk.js
+++ public/js/desk.js
@@ -1,43 +1,90 @@
 
 //Refencias HTML
 const lblPending = document.querySelector('#lbl-pending');
+const deskHeader = document.querySelector('h1');
+const noMoreAlert = document.querySelector('.alert');
+const searchParams = new URLSearchParams(window.location.search);
+const lblCurrentTicket = document.querySelector('small');
 
-  async function loadTotalTicketsCount() {
-    const pending = await fetch('/api/ticket/pending').then(res=>res.json());    
-    lblPending.innerHTML = pending.tickets.length;
+const btnDraw = document.querySelector('#btn-draw');
+const btnDone = document.querySelector('#btn-done');
+
+
+let workingTicket = null;
+
+async function getTicket() {
+
+  const { status, message, ticket } = await fetch(`/api/ticket/draw/${numberDesk}`).then(res => res.json());
+  workingTicket = ticket;
+  if (status == "error") {
+    lblCurrentTicket.innerHTML = message;;
+    return;
   }
+  lblCurrentTicket.innerHTML = ticket.number;
 
-  loadTotalTicketsCount();
-  connectToWebSockets();
+}
+
+async function finishTicket() {
+  if (!workingTicket) {
+    return;
+  }
+  await fetch(`/api/ticket/done/${workingTicket.id}`, { method: 'PUT' }).then(res => res.json());
+  lblCurrentTicket.innerHTML = "Nadie";
+  workingTicket = null;
+}
 
 
+if (!searchParams.has('escritorio')) {
+  window.location.href = 'index.html';
+}
+const numberDesk = searchParams.get('escritorio');
+deskHeader.innerHTML = numberDesk;
 
+function checkTicketCount(currentCount = 0) {
+  if (currentCount > 0) {
+    noMoreAlert.classList.add("d-none");
+  } else {
+    noMoreAlert.classList.remove("d-none");
+  }
+  lblPending.innerHTML = currentCount;
+}
 
+async function loadTotalTicketsCount() {
+  const pending = await fetch('/api/ticket/pending').then(res => res.json());
+  lblPending.innerHTML = pending.tickets.length;
+  checkTicketCount(pending.tickets.length);
+}
+loadTotalTicketsCount();
+connectToWebSockets();
 
-  function connectToWebSockets() {
+function connectToWebSockets() {
 
-  const socket = new WebSocket( 'ws://localhost:3000/ws' );
+  const socket = new WebSocket('ws://localhost:3000/ws');
 
-  socket.onmessage = ( message ) => {
-    //On-ticket-count-change    
-    const {type,payload}= JSON.parse(message.data);
-    if (type === 'on-newTicket') {        
-         loadTotalTicketsCount();
-    }        
+  socket.onmessage = (message) => {
+    //on-newTicket  
+    const { type, payload } = JSON.parse(message.data);
+    if (type === 'on-ticketNumberChanged') {
+      checkTicketCount(payload);
+    }
   };
 
-  socket.onclose = ( event ) => {
-    console.log( 'Connection closed' );
-    setTimeout( () => {
-      console.log( 'retrying to connect' );
+  socket.onclose = (event) => {
+    console.log('Connection closed');
+    setTimeout(() => {
+      console.log('retrying to connect');
       connectToWebSockets();
-    }, 1500 );
+    }, 1500);
 
   };
 
-  socket.onopen = ( event ) => {
-    console.log( 'Connected' );
+  socket.onopen = (event) => {
+    console.log('Connected');
   };
 
 }
+
+
+btnDraw.addEventListener('click', getTicket);
+btnDone.addEventListener('click', finishTicket);
 
--- src/presentation/services/ticket.service.ts
+++ src/presentation/services/ticket.service.ts
@@ -3,106 +3,73 @@ import { Ticket } from "../../domain/interfaces/tickets";
 import { WssService } from "./wss.services";
 
 export class TicketService {
-constructor(private readonly wsService= WssService.instance){
+    constructor(private readonly wsService = WssService.instance) {
 
-}
+    }
 
-    public readonly tickets: Ticket[] = [
-        {
-            id: UuidAdapter.v4(),
-            number: 1,
-            createAt: new Date(),
-            done: false
-        },
-        {
-            id: UuidAdapter.v4(),
-            number: 2,
-            createAt: new Date(),
-            done: false
-        },
-        {
-            id: UuidAdapter.v4(),
-            number: 3,
-            createAt: new Date(),
-            done: false
-        },
-        {
-            id: UuidAdapter.v4(),
-            number: 4,
-            createAt: new Date(),
-            done: false
-        },
-        {
-            id: UuidAdapter.v4(),
-            number: 5,
-            createAt: new Date(),
-            done: false
-        },
-        {
-            id: UuidAdapter.v4(),
-            number: 6,
-            createAt: new Date(),
-            done: false
-        },
-        {
-            id: UuidAdapter.v4(),
-            number: 1,
-            createAt: new Date(),
-            done: false
-        },
-    ];
-
-    private readonly workingOnTickets: Ticket[] = [];
-
-    
+     readonly tickets: Ticket[] = [];
     public get lastWorkingOnTickets(): Ticket[] {
-        return this.workingOnTickets.splice(0, 4);
+        return this.tickets.splice(0, 4);
     }
 
     public get pendingTickets(): Ticket[] {
         return this.tickets.filter(ticket => !ticket.handleAtDesk);
     }
 
-    public  get lastTicketNumber(): number {
+
+
+    public get lastTicketNumber(): number {
         return this.tickets.length > 0 ? this.tickets.at(-1)!.number : 0;
     }
 
-    public createTicket():Ticket {
+    public createTicket(): Ticket {
         const ticket = {
             id: UuidAdapter.v4(),
-            number: this.lastTicketNumber + 1,
-            createAt: new Date(),
+            number: this.lastTicketNumber + 1,            
+            handleAtDesk:undefined,
+            createAt: new Date(),            
             done: false
         };
         this.tickets.push(ticket);
-        this.wsService.sendMessage('on-newTicket', ticket);
-        return ticket;
-        //Notificar que hay un nuevo ticket
+        this.onTicketNumberChanged();
+        return ticket;        
     }
 
     public drawTicket(desk: string) {
         const ticket = this.tickets.find(ticket => !ticket.handleAtDesk);
         if (!ticket)
             return { status: 'error', message: "No hay tickets disponibles" };
-        const ticketAsignedPrevius =this.workingOnTickets.find(t=>t.handleAtDesk==desk && t.done==false)
-        if (ticketAsignedPrevius){
-            return { status: 'error', message: `Ya se encuentra trabajando con el  en ticket ${ticketAsignedPrevius.number}` };        
+        const ticketAsignedPrevius = this.tickets.find(t => t.handleAtDesk == desk && t.done == false)
+        console.log(ticketAsignedPrevius)
+        if (ticketAsignedPrevius) {
+            return { status: 'error', ticket: ticketAsignedPrevius, message: `Ya se encuentra trabajando con el  en ticket ${ticketAsignedPrevius.number}` };
         }
         ticket.handleAtDesk = desk;
         ticket.handleAt = new Date();
-        this.workingOnTickets.unshift({ ...ticket });
-
+        this.tickets.unshift({ ...ticket });
+        this.onTicketNumberChanged();
         return { status: 'ok', ticket };
     }
 
     public onFinishedTicket(id: string) {
         const ticket = this.tickets.find(ticket => ticket.id === id);
+        console.log(ticket)
         if (!ticket)
             return { status: 'error', message: "No existe el ticket" };
-        ticket.done = true;
-        return { status: 'ok', ticket };
+
+        this.tickets.map(t => {
+            if (t.id === id) {
+                t.done = true;
+            }
+            return t;
+        })            
+        return { status: 'ok' };
 
 
+    }
+
+    private onTicketNumberChanged() {
+        this.wsService.sendMessage('on-ticketNumberChanged', this.pendingTickets.length);
     }
 
 }
